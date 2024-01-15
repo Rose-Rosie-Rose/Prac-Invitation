d40d2597b1069060f37100038abb8eb10ea9dce5
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-01-15 22:57:28 +0900

    feat: 배경음악 재생 및 멈춤

--- src/components/backgroundMusic/index.js
+++ src/components/backgroundMusic/index.js
@@ -1,3 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+
 export const BackgroundMusic = () => {
-  return <>BackgroundMusic</>;
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    audio.addEventListener("canplay", () => {
+      if (isPlaying) {
+        audio.play();
+      }
+    });
+    return () => {
+      audio.removeEventListener("canplay", () => {});
+    };
+  }, [isPlaying]);
+
+  const handleTogglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current
+        .play()
+        .catch((error) =>
+          console.error("Error attempting to play audio:", error)
+        );
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <>
+      <button onClick={handleTogglePlay}>{isPlaying ? "멈춤" : "재생"}</button>
+      <audio ref={audioRef} loop>
+        <source src="/assets/backgroundMusic.mp3" type="audio/mp3" />
+      </audio>
+    </>
+  );
 };
